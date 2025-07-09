79a965fee3760bf59f9f98d9a928ad6d4ecb4b3c
Elijah M. Immer <me@eimmer.me>
2025-07-09 16:30:19 -0700

    Move sky out of main and start random source

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 mod coords;
 use coords::*;
 
+mod sky;
+use sky::SkyPlugin;
+
 use bevy::{
     asset::LoadedFolder, prelude::*, sprite::Anchor,
     text::FontSmoothing,
@@ -31,12 +34,13 @@ fn main() {
                 .set(ImagePlugin::default_nearest())
                 .set(WindowPlugin {
                     primary_window: Some(Window {
-                        title: "KhImNgu Game".into(),
+                        title: "RaMmYen Game".into(),
                         ..default()
                     }),
                     ..default()
                 }),
         ) // fallback to nearest sampling
+        // foreign plugins
         .add_plugins(PixelCameraPlugin)
         .add_plugins(TilemapPlugin)
         .add_plugins(FpsOverlayPlugin {
@@ -57,56 +61,57 @@ fn main() {
                     enabled: true,
                 },
             })
+        .add_plugins(SkyPlugin)
+        .init_resource::<RandomSource>()
+        .init_resource::<SpriteFolder>()
         .init_state::<AppState>()
-        .add_systems(OnEnter(AppState::Setup), init_resources)
-        .add_systems(Update, check_textures.run_if(in_state(AppState::Setup)))
-        .add_systems(
-            OnEnter(AppState::Finished),
-            (setup_camera, spawn_floors, spawn_sky),
-        )
-        .add_systems(Update, sky_movement.run_if(in_state(AppState::Finished)))
+        .add_systems(Startup, print_monitor_size)
+        .add_systems(Update, check_textures.run_if(in_state(AppState::Loading)))
+        .add_systems(OnEnter(AppState::Playing), (setup_camera, spawn_floors))
         .run();
+}
+
+fn print_monitor_size(
+    winit_windows: NonSend<WinitWindows>,
+    window_entity: Single<Entity, With<PrimaryWindow>>,
+) {
+    if let Some(monitor) = winit_windows
+        .get_window(*window_entity)
+        .and_then(|a| a.current_monitor())
+    {
+        info!("{:?}", monitor.size());
+    }
 }
 
 #[derive(Debug, Clone, Copy, Default, PartialEq, Eq, Hash, States)]
 enum AppState {
     #[default]
-    Setup,
-    Finished,
-}
-
-/// Indicates a tile is a sky tile
-#[derive(Component)]
-struct SkyTile;
-
-#[derive(Component)]
-struct SkyTileMap;
-
-#[derive(Resource)]
-struct SkyMovement {
-    /// The speed of movement in tiles per second, in axial coordinates.
-    speed: Vec2,
+    Loading,
+    Playing,
 }
 
 /// The source of randomness used by this example.
 #[derive(Resource)]
 struct RandomSource(SmallRng);
-
-/// The asset folder holding all the sprite sheets.
-#[derive(Resource)]
-struct SpriteFolder(Handle<LoadedFolder>);
-
-fn init_resources(mut commands: Commands, asset_server: Res<AssetServer>) {
-    // Load multiple, individual sprites from a folder
-    commands.insert_resource(SpriteFolder(asset_server.load_folder("sprites")));
-
-    commands.insert_resource(SkyMovement {
-        speed: Vec2::new(5., 3.),
-    });
-
-    commands.insert_resource(RandomSource(SmallRng::from_os_rng()));
+impl Default for RandomSource {
+    fn default() -> Self {
+        Self(SmallRng::from_os_rng())
+    }
 }
 
+/// The asset folder holding all the sprite sheets.
+#[derive(Deref, Resource)]
+struct SpriteFolder(Handle<LoadedFolder>);
+impl FromWorld for SpriteFolder {
+    fn from_world(world: &mut World) -> Self {
+        let asset_server = world.resource::<AssetServer>();
+        Self(asset_server.load_folder("sprites"))
+    }
+}
+
+/// Stalls the game until all of the textures have been loaded.
+///
+/// Is this necessary?
 fn check_textures(
     mut next_state: ResMut<NextState<AppState>>,
     sprite_folder: Res<SpriteFolder>,
@@ -115,227 +120,73 @@ fn check_textures(
     // Advance the `AppState` once all sprite handles have been loaded by the `AssetServer`
     for event in events.read() {
         if event.is_loaded_with_dependencies(&sprite_folder.0) {
-            next_state.set(AppState::Finished);
+            next_state.set(AppState::Playing);
         }
     }
 }
 
 fn setup_camera(mut commands: Commands) {
-    commands.spawn((
-        Camera2d,
-        PixelZoom::FitSize {
-            width: SCREEN_WIDTH as i32,
-            height: SCREEN_HEIGHT as i32,
-        },
-        PixelViewport,
-    ));
+    commands.spawn((Camera2d, PixelZoom::Fixed(2), PixelViewport));
 }
 
-fn spawn_floors(
-    mut commands: Commands,
-    asset_server: Res<AssetServer>,
-    mut texture_atlas_layouts: ResMut<Assets<TextureAtlasLayout>>,
-) {
-    // Create texture atlases with different padding and sampling
+fn spawn_floors(mut commands: Commands, asset_server: Res<AssetServer>) {
     let floor_texture = asset_server.load("sprites/basic_sheet.png");
-    let floor_layout = TextureAtlasLayout::from_grid(
-        HEX_SIZE.as_uvec2(),
-        FLOOR_TILE_ATLAS_HEIGHT,
-        FLOOR_TILE_ATLAS_WIDTH,
-        FLOOR_TILE_PADDING,
-        None,
-    );
-    let floor_texture_atlas_layout = texture_atlas_layouts.add(floor_layout);
 
-    spawn_section(
-        &mut commands,
-        IVec2::ZERO,
-        floor_texture.clone(),
-        floor_texture_atlas_layout.clone(),
-    );
-
-    Direction::ALL
-        .iter()
-        .map(|dir| dir.as_ivec2() * 4)
-        .for_each(|trans| {
-            spawn_section(
-                &mut commands,
-                trans,
-                floor_texture.clone(),
-                floor_texture_atlas_layout.clone(),
-            );
-        });
+    [
+        AxialPos::new(0, 0),
+        AxialPos::new(4, 0),
+        AxialPos::new(0, 4),
+        AxialPos::new(0, -4),
+        AxialPos::new(-4, 0),
+        AxialPos::new(4, -4),
+        AxialPos::new(-4, 4),
+    ]
+    .iter()
+    .for_each(|trans| {
+        spawn_section(&mut commands, *trans, floor_texture.clone());
+    });
 }
 
-fn spawn_section(
-    commands: &mut Commands,
-    center: IVec2,
-    texture: Handle<Image>,
-    layout: Handle<TextureAtlasLayout>,
-) {
-    let sprite = Sprite {
-        image: texture.clone(),
-        texture_atlas: Some(TextureAtlas {
-            layout: layout.clone(),
-            index: 0,
-        }),
-        anchor: Anchor::Center,
-        ..Default::default()
-    };
-    commands.spawn((
-        sprite.clone(),
-        Transform::from_translation(center.as_vec2().extend(0.)),
-    ));
+fn spawn_section(commands: &mut Commands, origin: AxialPos, texture: Handle<Image>) {
+    let map_size = TilemapSize { x: 1, y: 2 };
+    let center = (origin - AxialPos { q: 1, r: 0 }).center_in_world_row(&HEX_SIZE.as_vec2().into());
 
-    Direction::ALL
-        .iter()
-        .map(|dir| (center + dir.as_ivec2()).as_vec2().extend(0.))
-        .for_each(|trans| {
-            commands.spawn((sprite.clone(), Transform::from_translation(trans)));
-        });
-}
-
-fn spawn_sky(
-    mut commands: Commands,
-    asset_server: Res<AssetServer>,
-    mut rng: ResMut<RandomSource>,
-) {
-    let texture_handle: Handle<Image> = asset_server.load("sprites/sky_sheet.png");
-
+    let mut tile_storage = TileStorage::empty(map_size);
     let tilemap_entity = commands.spawn_empty().id();
-    let mut tile_storage = TileStorage::empty(SKY_MAP_SIZE);
+    let tilemap_id = TilemapId(tilemap_entity);
+    let hex_coord_system = HexCoordSystem::Row;
+    let map_type = TilemapType::Hexagon(hex_coord_system);
 
-    for x in 0..SKY_MAP_SIZE.x {
-        for y in 0..SKY_MAP_SIZE.y {
-            let tile_pos = TilePos { x, y };
-            let tile_entity = commands
-                .spawn((
-                    TileBundle {
-                        position: tile_pos,
-                        tilemap_id: TilemapId(tilemap_entity),
-                        texture_index: TileTextureIndex(rng.0.random_range(0..SKY_TILE_VARIENT_COUNT)),
-                        //color: TileColor(Color::srgba(0.0, 0.0, 0.0, 1.0)),
-                        ..Default::default()
-                    },
-                    SkyTile,
-                ))
+    let tile_positions = generate_hexagon(
+        AxialPos::from_tile_pos_given_coord_system(&TilePos { x: 1, y: 1 }, hex_coord_system),
+        1,
+    )
+    .into_iter()
+    .map(|axial_pos| axial_pos.as_tile_pos_given_coord_system(hex_coord_system));
+
+    commands.entity(tilemap_id.0).with_children(|parent| {
+        for tile_pos in tile_positions {
+            let tile_entity = parent
+                .spawn(TileBundle {
+                    position: tile_pos,
+                    tilemap_id,
+                    texture_index: TileTextureIndex(0),
+                    ..Default::default()
+                })
                 .id();
-            tile_storage.set(&tile_pos, tile_entity);
+            tile_storage.checked_set(&tile_pos, tile_entity)
         }
-    }
+    });
 
-    let grid_size = SKY_TILE_SIZE.into();
-    let map_type = TilemapType::Hexagon(HexCoordSystem::Row);
-
-    commands.entity(tilemap_entity).insert((
-        TilemapBundle {
-            grid_size,
-            map_type,
-            size: SKY_MAP_SIZE,
-            storage: tile_storage,
-            texture: TilemapTexture::Single(texture_handle),
-            tile_size: SKY_TILE_SIZE,
-            anchor: TilemapAnchor::Center,
-            transform: Transform::from_xyz(0., 0., SKY_TILE_LAYER),
-            ..Default::default()
-        },
-        SkyTileMap,
-    ));
+    commands.entity(tilemap_entity).insert(TilemapBundle {
+        grid_size: HEX_SIZE.as_vec2().into(),
+        size: map_size,
+        storage: tile_storage,
+        texture: TilemapTexture::Single(texture),
+        tile_size: HEX_SIZE.as_vec2().into(),
+        map_type,
+        anchor: TilemapAnchor::Center,
+        transform: Transform::from_translation(center.extend(0.)),
+        ..Default::default()
+    });
 }
-
-fn sky_movement(
-    time: Res<Time<Fixed>>,
-    sky_movement: ResMut<SkyMovement>,
-    mut rng: ResMut<RandomSource>,
-    mut tilemap: Single<(&TileStorage, &TilemapSize, &mut Transform), With<SkyTileMap>>,
-    mut tile_query: Query<&mut TileTextureIndex, With<SkyTile>>,
-) {
-    let map_size = IVec2::new(tilemap.1.x as i32, tilemap.1.y as i32);
-    let tile_storage = tilemap.0;
-
-    let old_translation = tilemap.2.translation.xy();
-    let mut new_translation =
-        AXIAL_TRANSLATION_MATRIX * sky_movement.speed * time.delta_secs() + old_translation;
-
-    let tile_diff = (new_translation / SKY_TILE_SIZE_LOOP_THRESHOLD)
-        .trunc()
-        .as_ivec2();
-
-
-    if tile_diff != IVec2::ZERO {
-        let flip_x = tile_diff.x > 0;
-        let flip_y = tile_diff.y > 0;
-
-        for y in 0..map_size.y {
-            let y = flip_y.then_some(map_size.y - y - 1).unwrap_or(y);
-            for x in 0..map_size.x {
-                let x = flip_x.then_some(map_size.x - x - 1).unwrap_or(x);
-
-                let old_pos = IVec2 { x, y };
-                // for the hexagons to align with where you started, they have
-                // to move 1.5 hexes up or 1 hex to the right.
-                // This does the 1.5 hexes up adjustment to turn the
-                // hex distance into square distance used by the position.
-                let adjusted_diff = (Mat2::from_cols_array_2d(&[
-                    [1., 0.],
-                    [-1., 2.]
-                ]) * tile_diff.as_vec2()).as_ivec2();
-                let replace_pos = old_pos + adjusted_diff;
-                let new_pos = old_pos - adjusted_diff;
-
-                let Some(curr_tile_entity) = tile_storage.get(&TilePos {
-                    x: x as u32,
-                    y: y as u32,
-                }) else {
-                    continue;
-                };
-
-                if replace_pos.cmpge(IVec2::ZERO).all() && replace_pos.cmplt(map_size).all() {
-                    // move the texture along the `tile_diff` vector
-
-                    let Some(new_tile_entity) = tile_storage.get(&TilePos {
-                        x: replace_pos.x as u32,
-                        y: replace_pos.y as u32,
-                    }) else {
-                        continue;
-                    };
-
-                    let Ok(curr_tile_texture) =
-                        tile_query.get(curr_tile_entity).and_then(|t| Ok(*t))
-                    else {
-                        continue;
-                    };
-
-                    match tile_query.get_mut(new_tile_entity) {
-                        Ok(mut new_tile_texture) => *new_tile_texture = curr_tile_texture,
-                        Err(err) => warn!("Failed to find replacing sky tile at {replace_pos} with {err}"),
-                    }
-                } else {
-                }
-
-                if new_pos.cmplt(IVec2::ZERO).any() || new_pos.cmpge(map_size).any() {
-                    match tile_query.get_mut(curr_tile_entity) {
-                        Ok(mut curr_tile_texture) => {
-                            let tile_idx = rng.0.random_range(0..SKY_TILE_VARIENT_COUNT);
-                            *curr_tile_texture = TileTextureIndex(tile_idx);
-                        }
-                        Err(err) => warn!("Failed to get current tile at {new_pos} with {err}"),
-                    };
-                }
-            }
-        }
-    }
-
-    new_translation -= tile_diff.as_vec2() * SKY_TILE_SIZE_LOOP_THRESHOLD;
-
-    tilemap.2.translation = new_translation.extend(tilemap.2.translation.z);
-}
-
-const SKY_MAP_SIZE: TilemapSize = TilemapSize { x: 16, y: 12 };
-const SKY_TILE_SIZE: TilemapTileSize = TilemapTileSize { x: 48., y: 52. };
-const SKY_TILE_SIZE_LOOP_THRESHOLD: Vec2 = Vec2 {
-    x: SKY_TILE_SIZE.x,
-    y: SKY_TILE_SIZE.y * 1.5,
-};
-const SKY_TILE_LAYER: f32 = -1.;
-const SKY_TILE_VARIENT_COUNT: u32 = 8;
---
+++ src/sky.rs
@@ -0,0 +1,191 @@
+//! The infinite sky implementation
+use crate::RandomSource;
+use crate::coords::*;
+
+use bevy::{asset::LoadedFolder, prelude::*};
+use bevy_ecs_tilemap::helpers::hex_grid::axial::AxialPos;
+use bevy_ecs_tilemap::prelude::*;
+use bevy_pixcam::{PixelCameraPlugin, PixelViewport, PixelZoom};
+use rand::{prelude::*, rngs::SmallRng};
+
+pub struct SkyPlugin;
+
+impl Plugin for SkyPlugin {
+    fn build(&self, app: &mut App) {
+        app.init_resource::<SkyMovement>()
+            .add_systems(Startup, spawn_sky)
+            .add_systems(Update, sky_movement);
+    }
+}
+
+/// Indicates a tile is a sky tile
+#[derive(Component)]
+struct SkyTile;
+
+#[derive(Component)]
+struct SkyTileMap;
+
+#[derive(Resource)]
+struct SkyMovement {
+    /// The speed of movement in tiles per second, in axial coordinates.
+    speed: Vec2,
+}
+impl Default for SkyMovement {
+    fn default() -> Self {
+        Self {
+            speed: Vec2::new(5., 2.),
+        }
+    }
+}
+
+/// Spawns the sky fitting the screen (to an extent).
+fn spawn_sky(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut rng: ResMut<RandomSource>,
+) {
+    let texture_handle: Handle<Image> = asset_server.load("sprites/sky_sheet.png");
+
+    let tilemap_entity = commands.spawn_empty().id();
+    let mut tile_storage = TileStorage::empty(SKY_MAP_SIZE);
+
+    for x in 0..SKY_MAP_SIZE.x {
+        for y in 0..SKY_MAP_SIZE.y {
+            let tile_pos = TilePos { x, y };
+            commands.entity(tilemap_entity).with_children(|parent| {
+                let tile_entity = parent
+                    .spawn((
+                        TileBundle {
+                            position: tile_pos,
+                            tilemap_id: TilemapId(tilemap_entity),
+                            texture_index: TileTextureIndex(
+                                rng.0.random_range(0..SKY_TILE_VARIENT_COUNT),
+                            ),
+                            //color: TileColor(Color::srgba(0.0, 0.0, 0.0, 1.0)),
+                            ..Default::default()
+                        },
+                        SkyTile,
+                    ))
+                    .id();
+                tile_storage.set(&tile_pos, tile_entity);
+            });
+        }
+    }
+
+    commands.entity(tilemap_entity).insert((
+        TilemapBundle {
+            grid_size: SKY_TILE_SIZE.into(),
+            map_type: TilemapType::Hexagon(HexCoordSystem::Row),
+            size: SKY_MAP_SIZE,
+            storage: tile_storage,
+            texture: TilemapTexture::Single(texture_handle),
+            tile_size: SKY_TILE_SIZE,
+            anchor: TilemapAnchor::Center,
+            transform: Transform::from_xyz(0., 0., SKY_TILE_LAYER),
+            ..Default::default()
+        },
+        SkyTileMap,
+    ));
+}
+
+/// Moves the sky with an illusion that it is indefinite.
+///
+/// This system
+///
+fn sky_movement(
+    time: Res<Time>,
+    sky_movement: ResMut<SkyMovement>,
+    mut rng: ResMut<RandomSource>,
+    mut tilemap: Single<(&TileStorage, &TilemapSize, &mut Transform), With<SkyTileMap>>,
+    mut tile_query: Query<&mut TileTextureIndex, With<SkyTile>>,
+) {
+    let map_size: IVec2 = IVec2::new(tilemap.1.x as i32, tilemap.1.y as i32);
+    let tile_storage = tilemap.0;
+
+    let old_translation = tilemap.2.translation.xy();
+    let mut new_translation =
+        AXIAL_TRANSLATION_MATRIX * sky_movement.speed * time.delta_secs() + old_translation;
+
+    let tile_diff = (new_translation / SKY_TILE_SIZE_LOOP_THRESHOLD)
+        .trunc()
+        .as_ivec2();
+
+    // only translate by the sky by the amount that was less than a whole tile.
+    new_translation -= tile_diff.as_vec2() * SKY_TILE_SIZE_LOOP_THRESHOLD;
+
+    tilemap.2.translation = new_translation.extend(tilemap.2.translation.z);
+
+    if tile_diff == IVec2::ZERO { return; }
+
+    let flip_x = tile_diff.x > 0;
+    let flip_y = tile_diff.y > 0;
+
+    for y in 0..map_size.y {
+        let y = flip_y.then_some(map_size.y - y - 1).unwrap_or(y);
+        for x in 0..map_size.x {
+            let x = flip_x.then_some(map_size.x - x - 1).unwrap_or(x);
+
+            let old_pos = IVec2 { x, y };
+
+            // for the hexagons to align with where you started, they have
+            // to move 1.5 hexes up or 1 hex to the right.
+            // This does the 1.5 hexes up adjustment to turn the
+            // hex distance into square distance used by the position.
+            let adjusted_diff =
+                (Mat2::from_cols_array(&[1., 0., -1., 2.]) * tile_diff.as_vec2()).as_ivec2();
+
+            let replace_pos = old_pos + adjusted_diff;
+            let new_pos = old_pos - adjusted_diff;
+
+            let Some(curr_tile_entity) = tile_storage.get(&old_pos.as_uvec2().into()) else {
+                warn!("Failed to find sky tile entity at position ({x}, {y})");
+                continue;
+            };
+
+            if replace_pos.cmpge(IVec2::ZERO).all() && replace_pos.cmplt(map_size).all() {
+                // move the texture along the `tile_diff` vector
+
+                let Some(new_tile_entity) = tile_storage.get(&replace_pos.as_uvec2().into())
+                else {
+                    warn!("Failed to find new tile at pos {replace_pos}");
+                    continue;
+                };
+
+                let curr_tile_texture =
+                    match tile_query.get(curr_tile_entity).and_then(|t| Ok(*t)) {
+                        Ok(curr_tile_texture) => curr_tile_texture,
+                        Err(err) => {
+                            warn!("Failed to find base sky tile at {old_pos} with {err}");
+                            continue;
+                        }
+                    };
+
+                match tile_query.get_mut(new_tile_entity) {
+                    Ok(mut new_tile_texture) => *new_tile_texture = curr_tile_texture,
+                    Err(err) => warn!(
+                        "Failed to find to be replaced sky tile at {replace_pos} with {err}"
+                    ),
+                }
+            }
+
+            if new_pos.cmplt(IVec2::ZERO).any() || new_pos.cmpge(map_size).any() {
+                match tile_query.get_mut(curr_tile_entity) {
+                    Ok(mut curr_tile_texture) => {
+                        let tile_idx = rng.0.random_range(0..SKY_TILE_VARIENT_COUNT);
+                        *curr_tile_texture = TileTextureIndex(tile_idx);
+                    }
+                    Err(err) => warn!("Failed to get current tile at {new_pos} with {err}"),
+                };
+            }
+        }
+    }
+}
+
+const SKY_MAP_SIZE: TilemapSize = TilemapSize { x: 40, y: 24 };
+const SKY_TILE_SIZE: TilemapTileSize = TilemapTileSize { x: 48., y: 52. };
+const SKY_TILE_SIZE_LOOP_THRESHOLD: Vec2 = Vec2 {
+    x: SKY_TILE_SIZE.x,
+    y: SKY_TILE_SIZE.y * 1.5,
+};
+const SKY_TILE_LAYER: f32 = -1.;
+const SKY_TILE_VARIENT_COUNT: u32 = 8;
