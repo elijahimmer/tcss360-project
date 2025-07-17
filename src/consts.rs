98feb7634cd3164d2d1b076b022f050d5fb9c30a
Elijah M. Immer <me@eimmer.me>
2025-07-17 13:24:03 -0700

    Add more tiles

--- src/consts.rs
+++ src/consts.rs
@@ -3,5 +3,5 @@ use std::ops::Range;
 
 pub const TILE_SIZE: TilemapTileSize = TilemapTileSize { x: 48.0, y: 52.0 };
 pub const TILE_ASSET_LOAD_PATH: &'static str = "embedded://assets/sprites/basic_sheet.png";
-pub const FLOOR_TILE_VARIENTS: Range<u32> = 0..1;
-pub const SKY_TILE_VARIENTS: Range<u32> = 1..9;
+pub const FLOOR_TILE_VARIENTS: Range<u32> = 0..6;
+pub const SKY_TILE_VARIENTS: Range<u32> = 6..14;
